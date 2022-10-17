ce53fe662ed9c31a4133afdab609b0e76b405620
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2022-10-17 20:27:18 +0000

    Fix linters errors

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector('.button');
 const menu = document.querySelector('.menu');
 const nav = document.querySelector('nav');
-let logo = document.querySelector('.logo');
+const logo = document.querySelector('.logo');
 
 btn.addEventListener('click', () => {
   menu.classList.toggle('active');
