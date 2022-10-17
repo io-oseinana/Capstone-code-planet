e3a9f75bad6a5cfcdb18e0e0b1a70b29fa8abba7
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2022-10-17 21:35:27 +0000

    Add class to body to avoid overflow when menu is active

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ const btn = document.querySelector('.button');
 const menu = document.querySelector('.menu');
 const nav = document.querySelector('nav');
 const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
 
 btn.addEventListener('click', () => {
   menu.classList.toggle('active');
   nav.classList.toggle('active');
   btn.classList.toggle('active');
   logo.classList.toggle('x');
+  body.classList.toggle('active');
 });
